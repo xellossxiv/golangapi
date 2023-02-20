819070e60d7f5d8a0a70da641cb28b36f023d97c
xellossxiv <dwiputra.52@gmail.com>
2023-02-20 22:00:39 +0700

    Delete tools directory

--- tools/ErrorCode.go
+++
@@ -1,8 +0,0 @@
-package tools
-
-import "fmt"
-
-func ErrorCode() {
-	C500 := "A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond."
-	fmt.Println(C500)
-}
