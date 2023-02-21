d4ab3a1d0cf091a9f2fbc90eaaf754088ff7a491
xellossxiv <dwiputra.52@gmail.com>
2023-02-21 18:09:37 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.GET("", HelloServer)
 	router.POST("iamAPI/v1/aralia/setUser", setUserAralia)
 	router.POST("iamAPI/v1/hcis/setUser", setUserHcis)
 	router.Run("0.0.0.0:8081")
@@ -17,4 +20,8 @@ func setUserAralia(c *gin.Context) {
 
 func setUserHcis(c *gin.Context) {
 	SetUser(c, "hcis")
+}
+
+func HelloServer(c *gin.Context) {
+	c.IndentedJSON(http.StatusAccepted, &JsonMessage{"200", "Success", "User Processed to Sailpoint"})
 }
